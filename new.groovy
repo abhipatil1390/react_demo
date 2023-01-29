ed5faba6c9423ff59057838b2ce2157fd49f926e
abhipatil1390 <83583882+abhipatil1390@users.noreply.github.com>
2023-01-29 13:12:56 +0530

    Create new.groovy

---
+++ new.groovy
@@ -0,0 +1,28 @@
+pipeline {
+    agent any
+
+    stages {
+        stage('code') {
+           
+            steps {
+    
+                   git url: 'https://github.com/abhipatil1390/react_demo.git', branch: 'main'
+            }
+        }
+        stage('build') {
+            steps {
+                sh 'docker build . -t first:latest'
+            }
+        }
+        stage('test') {
+            steps {
+                echo "test has successfully!"
+            }
+        }
+        stage('deploy') {
+            steps {
+                sh 'docker run -d --name react_d -p 8001:8001 first:latest '
+            }
+        }
+    }
+}
